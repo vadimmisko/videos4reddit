42d4b1b698369e4436130a19522707c3481d7bbb
Vadim Misko <vadimmisko.dev@gmail.com>
2016-08-22 16:58:17 +0300

    redux devtools

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import promise from 'redux-promise';
 
-import reducers from './reducers';
+import reducer from './reducers';
 import routes from './routes';
 
 import '../style/style.css';
 
-const createStoreWithMiddleware = applyMiddleware(
-  promise
-)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+let store = createStore(reducer, compose(
+  applyMiddleware(promise),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container')
