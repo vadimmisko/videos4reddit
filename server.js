1a354cad3f6df20a841359e5bd7d69425bdb3b60
Vadim Misko <vadimmisko.dev@gmail.com>
2016-09-20 21:49:21 +0300

    Heroku config

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/signin'));
 
 app.listen(process.env.PORT || 8080);
