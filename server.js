950066f138daf53de79dca184574f27950568ff7
Vadim Misko <vadimmisko.dev@gmail.com>
2016-11-06 22:08:26 +0200

    Refactoring

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
+var express     = require('express');
 var compression = require('compression');
+
+var app   = express();
+var port  = process.env.PORT || 8080;
 
 app.use(compression());
 app.use(express.static(__dirname + '/'));
@@ -8,4 +10,6 @@ app.use(express.static(__dirname + '/'));
 app.use('/signin', express.static(__dirname + '/'));
 app.use('/post/:id', express.static(__dirname + '/'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, function () {
+  console.log('Server is flying on port:' + port)
+});
