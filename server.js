0f0ff4526905ded6371e6d80410771a9bd04875d
Vadim Misko <vadimmisko.dev@gmail.com>
2017-02-20 15:57:31 +0200

    Rendering routes on server

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express     = require('express');
+var path        = require('path');
 var compression = require('compression');
 
 var app   = express();
 var port  = process.env.PORT || 8080;
 
-app.use(compression());
-app.use(express.static(__dirname + '/'));
-// TODO: Make all routes be imported from ./src/routes.js
-app.use('/signin', express.static(__dirname + '/'));
-app.use('/post/:id', express.static(__dirname + '/'));
+app.use(express.static(path.join(__dirname, '/')))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/', 'index.html'))
+})
 
 app.listen(port, function () {
   console.log('Server is flying on port:' + port)
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { Router, browserHistory } from 'react-router';
 import promise from 'redux-promise';
+import { Router, browserHistory } from 'react-router';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-92289588-1');
 
 import reducer from './reducers';
 import routes from './routes';
 
 import '../style/style.css';
+
+function fireTracking() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -19,7 +26,7 @@ let store = createStore(reducer, compose(
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes} />
+    <Router onUpdate={fireTracking} history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container')
 );
